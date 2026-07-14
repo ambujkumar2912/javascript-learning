dade2cf6bef670a3b9b7ef2108cca6b7c6bafdd2
ambujkumar2912 <ambujkumar2912@gmail.com>
2026-07-14 08:28:10 +0530

    learning basic of javasript

---
+++ basic01/conversionoperation.js
@@ -0,0 +1,18 @@
+let score = 33
+
+console.log(score);
+
+console.log(typeof score)
+
+let a = String(score)
+
+console.log(typeof a);
+
+
+
+let num = false;
+
+let ab = Number(num);
+
+console.log(ab);        // "true"
+console.log(typeof ab); // string
