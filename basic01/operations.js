49698d19f396b70486d4c1cf2ce33bf76394c250
ambujkumar2912 <ambujkumar2912@gmail.com>
2026-07-14 09:40:57 +0530

    new topics of operations

---
+++ basic01/operations.js
@@ -0,0 +1,18 @@
+let value = 10 ;
+let negvalue = -value
+console.log(negvalue);
+
+let a = 10;
+let b = 5;
+
+console.log(a + b); // 15
+console.log(a - b); // 5
+console.log(a * b); // 50
+console.log(a / b); // 2
+console.log(a % b); // 0 (remainder)
+console.log(a ** b); // 100000 (10^5)
+
+let str1 = "Ambuj"
+let str2 = " kumar"
+let str3 = str1 + str2 
+console.log(str3);
