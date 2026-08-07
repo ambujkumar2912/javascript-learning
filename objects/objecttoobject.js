f38cc89548420eecd59d43ebd789e1d07257fd08
ambujkumar2912 <ambujkumar2912@gmail.com>
2026-08-07 20:36:44 +0530

    Added more practice files

---
+++ objects/objecttoobject.js
@@ -0,0 +1,20 @@
+let collage = [{
+
+    iit : {
+
+        grade : "a+" ,
+        naac  : "a++"
+    } }, 
+        {   nit : {
+       grade : "b+",
+       naac  : "c++"
+
+    } 
+
+        }
+  ];
+
+collage[1].grade = "a++++"
+
+console.log(collage[1].grade);
+
